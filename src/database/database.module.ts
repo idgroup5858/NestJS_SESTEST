7e1347ddd2c8f7497d2ff40ae0c38ea161626797
Fourcade7 <praminov7@gmail.com>
2026-08-18 10:43:40 +0500

    from pr

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,7 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         TypeOrmModule.forRoot({
             type:"postgres",
             host:"localhost",
-            port:5433,
+            port:5432, //from pr
             username:"postgres", 
             password:"root",     
             database:"prsestest",    
