4bae7246f9917a94a8ade0d4d4923288b2e48a16
idgroup5858 <idgroup5858@gmail.com>
2026-09-12 00:26:04 +0500

    sangig kassir

--- src/role/dto/create-role.dto.ts
+++ src/role/dto/create-role.dto.ts
@@ -2,7 +2,7 @@ import { IsIn } from "class-validator";
 
 export class CreateRoleDto {
 
-    @IsIn(['admin', 'kassir', 'lab_asistant', 'lab_director',"director","super_admin"])
+    @IsIn(['admin', 'kassir', 'lab_asistant', 'lab_director',"director","super_admin","kassir_sangig"])
     name:string;
     company_id:number;
 }
